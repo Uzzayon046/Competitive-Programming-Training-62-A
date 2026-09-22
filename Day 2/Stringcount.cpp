2b403fc9d58dca6199f7d94d094f3f47422d8e54
Uzzayon Biswas <uzzayonbiswas@gmail.com>
2026-09-22 14:47:10 +0600

    Add files via upload

---
+++ Day 2/Stringcount.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string fullname= "Uzzayon Biswas";
+
+
+    cout<<"a appeared in fullname "<<" "<<count(fullname.begin(),fullname.end(),'a');
+    
+    
+}
